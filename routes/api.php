adb047386a143bad305285cc11cbf5348162e37b
Jeyker <mendozajeyker1178@gmail.com>
2025-09-29 20:16:54 -0400

    Implement user creation and retrieval endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\UserController;
+
+Route::get('/users', [UserController::class, 'index']);
+
+Route::post('/user/create', [UserController::class, 'store']);
